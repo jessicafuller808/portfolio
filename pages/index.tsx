1668b3a89e4d8f8f6a2d22b2076406139b916a33
Jessica Fuller <jessicanfuller808@gmail.com>
2022-11-12 12:56:23 -0800

    created About page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import About from "../components/About";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="space-y-14 lg:space-y-24">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <main className="max-w-4xl mx-auto mt-16 antialiased">
+        <About/>
+      </main>
     </div>
   )
 }
